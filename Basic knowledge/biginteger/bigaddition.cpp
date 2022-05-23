b9aba18fdcf529ce0f6db7ef305038e0ccd0d2df
fyt <605325871@qq.com>
2022-05-23 22:41:50 +0800

    big

--- Basic knowledge/biginteger/bigaddition.cpp
+++ Basic knowledge/biginteger/bigaddition.cpp
@@ -2,19 +2,21 @@
 #include <vector>
 using namespace std;
 
-const int N = 1e6 + 10;
 vector<int> add(vector<int> &A, vector<int> &B)
 {
     vector<int> c;
-    int t=0;
-    for(int i=0; i<A.size()||i<B.size();i++)
+    int t = 0;
+    for (int i = 0; i < A.size() || i < B.size(); i++)
     {
-        if(i<A.size()) t+=A[i];
-        if(i<B.size()) t+=B[i];
-        c.push_back(t%10);
-        t/=10;
+        if (i < A.size())
+            t += A[i];
+        if (i < B.size())
+            t += B[i];
+        c.push_back(t % 10);
+        t /= 10;
     }
-    if(t) c.push_back(1);
+    if (t)
+        c.push_back(1);
     return c;
 }
 int main()
@@ -22,8 +24,11 @@ int main()
     string a, b;
     vector<int> A, B;
     cin >> a >> b;
-    for (int i = a.size() - 1; i >= 0; i--)   A.push_back(a[i] - '0');
-    for (int i = b.size() - 1; i >= 0; i--)   B.push_back(b[i] - '0');
-     auto c = add(A, B);
-    for (int i = c.size() - 1; i >= 0; i--)   printf("%d", c[i]);
+    for (int i = a.size() - 1; i >= 0; i--)
+        A.push_back(a[i] - '0');
+    for (int i = b.size() - 1; i >= 0; i--)
+        B.push_back(b[i] - '0');
+    auto c = add(A, B);
+    for (int i = c.size() - 1; i >= 0; i--)
+        printf("%d", c[i]);
 }
